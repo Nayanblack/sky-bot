c3a6421df606647a3c93aff5021ab964243c9dc8
Nayanblack <130187443+Nayanblack@users.noreply.github.com>
2023-05-26 01:15:23 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -9,4 +9,4 @@ exports.TEMP_DIR = path.resolve(__dirname, "..", "assets", "temp");
 
 exports.TIMEOUT_IN_MILLISECONDS_BY_EVENT = 700;
 
-exports.OPENAI_API_KEY = "coloque_aqui_seu_token_da_openai";
+exports.OPENAI_API_KEY = "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";
